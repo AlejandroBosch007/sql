152bb1c5d132293394fd45506259fe2126499506
AlejandroBosch007 <tecla@puma.unam.mx>
2021-09-10 14:00:25 -0500

    inicio

---
+++ routes/autor.routes.js
@@ -0,0 +1,24 @@
+const autorServices = require('../services/autor.services')
+
+module.exports = (app)=> {
+    app.get('/autor', async (req, res)=>{
+        try {
+            let resultado = await autorServices.listarAutor()
+            res.json(resultado)
+        }catch (err){
+            console.log(err.message)
+            res.status(500).json({error : err.message}) 
+        }
+    })
+    app.post('/autor', async (req,res)=>{
+        let autor = req.body
+        try {
+            let resultado = await librosServices.nuevoAutor(autor)
+            console.log(resultado)
+            res.json(resultado)
+        }catch (err){
+            console.log(err.message)
+            res.status(500).json({error : err.message})
+        }
+    })
+}
---
+++ services/autor.services.js
@@ -0,0 +1,24 @@
+const sequelize = require('../db/conexion')
+
+
+module.exports.nuevoAutor = async (autor)=> {
+    try {
+        let resultado = await sequelize.query(`INSERT INTO autor (nombres , apellidos) VALUES (?,?)`,
+        {replacements: autor, type: sequelize.QueryTypes.SELECT});
+        console.log(resultado)
+        return 'Alta de autor correctamente'
+    }catch (error) {
+        console.log(error)
+        throw new Error ("Ocurrio un error en la consulta");
+    }
+}
+
+module.exports.listarAutor = async ()=>{
+    try {
+        let resultado = await sequelize.query('SELECT * FROM autor')
+        return resultado
+    }catch (error){
+        console.log(error)
+        throw new Error ("Ocurrio un error en la consulta");
+    }
+}
